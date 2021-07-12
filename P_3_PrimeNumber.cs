b92ac585067e599b101d2d6fb75199cee5ac75d6
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-12 14:57:48 +0530

    [Shantanu] Added : Program to find Prime number

---
+++ P_3_PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P_3_PrimeNumber
+{
+    class P_3_PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            int N, i, remainder,a=0;
+            Console.Write("Enter number you want to check: ");
+            N = int.Parse(Console.ReadLine());
+
+
+            for (i=2;i<N;i++)
+            {
+                remainder = N % i;
+
+
+                if (remainder == 0)
+                {
+                    Console.WriteLine(+N + " is a not prime number.");
+                    a = 1;
+                    break;
+                }
+                
+            }
+            if (a==0)
+            {
+                Console.WriteLine(+N + " is a prime number.");
+            }
+
+        }
+    }
+}
