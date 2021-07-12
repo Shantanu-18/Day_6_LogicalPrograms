2bea2bbdf98d87f39aba94abce47f195c7c57330
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-12 14:54:37 +0530

    [Shantanu] Added : Program to find Fibonacci Series

---
+++ P_1_fibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P_1_fibonacciSeries
+{
+    class P_1_fibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1, num3, i, N;
+            Console.Write("Enter the N: ");
+
+
+            N = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+
+
+            for (i = 2; i < N; ++i)    
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
